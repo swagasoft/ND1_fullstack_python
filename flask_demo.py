bd143a1d99ec42e318fbf8542bc63faec8d3b836
Simon <swagasoft@gmail.com>
2022-05-21 21:01:13 +0100

    added migrations

--- flask_demo.py
+++ flask_demo.py
@@ -1,15 +1,28 @@
+from email.policy import default
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@127.0.0.1:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1:5432/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+migrate =  Migrate(app, db)
 
 class User(db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
+
+  def __repr__(self):
+      return f'<User {self.id}, {self.name}>'
+
+
+class Todos(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
 
   def __repr__(self):
       return f'<User {self.id}, {self.name}>'
